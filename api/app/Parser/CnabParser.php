e9c8627b0c2a0b4826294b4bf16f338217a70557
lincolnbrito <lincoln.sbrito@gmail.com>
2021-08-22 14:10:23 -0300

    chore: test parser can generate valid line result

--- api/app/Parser/CnabParser.php
+++ api/app/Parser/CnabParser.php
@@ -20,6 +20,29 @@ class CnabParser
     }
 
     /**
+     * @param string $line
+     * @return array|null
+     * @throws Exception
+     */
+    public function parseLine(string $line): ?array
+    {
+        if($line === '' || !self::isCnabHeader($line)) {
+            throw new Exception('Invalid line format');
+        }
+
+        return [
+            'transaction_type' => (int) $line[0],
+            'date' => substr($line, 1,8),
+            'amount' => (float)substr($line, 9, 10) / 100,
+            'document' => substr($line, 19, 11),
+            'credit_card' => substr($line, 30, 12),
+            'hour' => substr($line, 42, 6),
+            'owner' => trim(substr($line, 48, 14)),
+            'store' => trim(substr($line, 62, 19)),
+        ];
+    }
+
+    /**
      * @param $content
      * @return bool
      */
--- api/tests/Unit/CnabParserTest.php
+++ api/tests/Unit/CnabParserTest.php
@@ -16,15 +16,6 @@ class CnabParserTest extends TestCase
         parent::setUp();
     }
 
-    public function provideContent(): array
-    {
-        return [
-            'invalidSize' => [
-                ""
-            ],
-        ];
-    }
-
     /**
      * @throws Exception
      */
@@ -43,4 +34,70 @@ class CnabParserTest extends TestCase
         $content = "3201903010000014200096206760174753****3153";
         $this->parser->parseContent($content);
     }
+
+    public function test_it_can_parse_line()
+    {
+        $validContent = "3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       ";
+        $result = $this->parser->parseLine($validContent);
+        $this->assertIsArray($result);
+
+    }
+
+    /**
+     * @dataProvider provideLineContent
+     * @throws Exception
+     */
+    public function test_it_can_return_valid_parsed_line($line, $expected): void
+    {
+        $result = $this->parser->parseLine($line);
+
+        $expectedFormat = [
+            'transaction_type',
+            'date',
+            'amount',
+            'document',
+            'credit_card',
+            'hour',
+            'owner',
+            'store',
+        ];
+
+        $this->assertIsArray($result);
+        $this->assertEquals($expectedFormat, array_keys($result));
+        $this->assertEquals($expected, $result);
+    }
+
+    public function provideLineContent(): array
+    {
+        return [
+            'line1' => [
+                '3201903010000014200096206760174753****3153153453JOÃO MACEDO   BAR DO JOÃO       ',
+                [
+                    'transaction_type' => 3,
+                    'date' => '20190301',
+                    'amount' => 142.0,
+                    'document' => '09620676017',
+                    'credit_card' => '4753****3153',
+                    'hour' => '153453',
+                    'owner' => 'JOÃO MACEDO',
+                    'store' => 'BAR DO JOÃO',
+                ]
+            ],
+            'line2' => [
+                '5201903010000013200556418150633123****7687145607MARIA JOSEFINALOJA DO Ó - MATRIZ',
+                [
+                    "transaction_type" => 5,
+                    "date" => "20190301",
+                    "amount" => 132.0,
+                    "document" => "55641815063",
+                    "credit_card" => "3123****7687",
+                    "hour" => "145607",
+                    "owner" => "MARIA JOSEFINA",
+                    "store" => "LOJA DO Ó - MATRIZ"
+                ]
+            ]
+        ];
+    }
+
+
 }
