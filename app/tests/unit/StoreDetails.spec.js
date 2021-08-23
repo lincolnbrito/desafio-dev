c1ddbf2118d4906e50fa0db0f712158e0a1470b9
lincolnbrito <lincoln.sbrito@gmail.com>
2021-08-23 11:44:16 -0300

    chore: test StoreDetails component

--- app/tests/unit/StoreDetails.spec.js
+++ app/tests/unit/StoreDetails.spec.js
@@ -1,12 +1,35 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue';
+import StoreDetais from '@/components/StoreDetails.vue'
+import VueCurrencyFilter from "vue-currency-filter";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('StoreDetail.vue', () => {
+  Vue.use(VueCurrencyFilter,
+      {
+        symbol : ' R$',
+        thousandsSeparator: '.',
+        fractionCount: 2,
+        fractionSeparator: ',',
+        symbolPosition: 'front',
+        symbolSpacing: true,
+        avoidEmptyDecimals: undefined,
+      })
+
+  it('renders props.store when passed', () => {
+    const store = {
+      name: 'Store',
+      balance: -100,
+      owner: {
+        name: 'Owner'
+      }
+    }
+    const wrapper = shallowMount(StoreDetais, {
+      propsData: { store }
     })
-    expect(wrapper.text()).toMatch(msg)
+    const spans = wrapper.findAll('span')
+    const span = spans.at(0)
+    expect(wrapper.text()).toMatch('Store')
+    expect(wrapper.text()).toMatch('- R$ 100,00')
+    expect(span.classes()).toContain('danger')
   })
 })
